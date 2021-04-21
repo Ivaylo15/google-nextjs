de7e57022761d60f16cb2e6dd620e3b2179efe45
Ivaylo <ivaylopehlivanov@abv.bg>
2021-04-22 01:49:51 +0300

    input value fix

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,7 +1,6 @@
 import PaginationButtons from "./PaginationButtons"
 
 const SearchResults = ({ results }) => {
-    console.log(results)
     return (
         <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
             <p className="text-gray-600 text-md mb-5 mt-4">About {results.searchInformation?.formattedTotalResults} results ({results.searchInformation?.formattedSearchTime} seconds)</p>
